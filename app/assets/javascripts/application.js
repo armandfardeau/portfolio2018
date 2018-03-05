5d83eb5361a5f150e7526f21ffb8680b3dcd6664
armandfardeau <fardeauarmand@gmail.com>
2018-03-06 00:19:18 +0100

    move content of home page to about

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,30 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    previewEqualizer();
+});
+window.addEventListener("resize", function (e) {
+    previewEqualizer();
+});
+
+document.addEventListener('turbolinks:load', function () {
+    previewEqualizer();
+});
+
+function previewEqualizer() {
+    var previews = document.getElementsByClassName('portfolio_picture');
+    if (previews.length) {
+        var previews_container = document.getElementsByClassName('portfolio_picture-container');
+        var preview_content_min_heights = previews[0].height;
+        for (var i = 0; i < previews.length; i++) {
+            if (previews[i].height < preview_content_min_heights) {
+                preview_content_min_heights = previews[i].height;
+            }
+        }
+        for (var j = 0; j < previews_container.length; j++) {
+            previews_container[j].style.height = preview_content_min_heights + 'px';
+        }
+    }
+}
