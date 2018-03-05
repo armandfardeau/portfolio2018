97311a4fd41d8439d3a23963ff09fa03b738392a
armandfardeau <fardeauarmand@gmail.com>
2018-03-05 20:05:11 +0100

    add base controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'pages#home'
+  get 'pages/portfolio'
+  get 'pages/blog'
+  get 'pages/about'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
