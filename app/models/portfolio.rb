5187e7b5faf75fbe4b508831660c930d6404768b
armandfardeau <fardeauarmand@gmail.com>
2018-03-06 17:12:11 +0100

    add home page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
-  has_attached_file :picture, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png",
+                    :storage => :cloudinary,
+                    :path => ':id/:style/:filename'
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
