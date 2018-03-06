0e597c1c37f13ebd288d5caf934164722b19de02
armandfardeau <fardeauarmand@gmail.com>
2018-03-07 00:39:56 +0100

    add new size for single page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,10 @@
 class Portfolio < ApplicationRecord
-  has_attached_file :picture, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png",
+  has_attached_file :picture, styles: {
+      large: "600x600",
+      medium: "300x300>",
+      thumb: "100x100>"
+  },
+                    default_url: "/images/:style/missing.png",
                     :storage => :cloudinary,
                     :path => ':id/:style/:filename'
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
